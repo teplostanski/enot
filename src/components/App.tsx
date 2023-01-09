232e0349c3dd37498ac4d42fd166518ee7f241e3
Irog Teplostanski <teplostanski@yandex.ru>
2023-01-09 15:59:22 +0300

    add component App

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState('');
+
+  return <div>aned</div>;
+};
+
+export default App;
