00ce02e1a7914766b6c0fdb21e0e842db255e934
teplostanski <teplostanski@yandex.ru>
2023-02-05 23:47:15 +0300

    feat: add AllNotes component

---
+++ src/components/AllNotes/AllNotes.tsx
@@ -0,0 +1,61 @@
+interface AllNotesProps {
+  onAddNote: any;
+  notes: any;
+  onDeleteNote: any;
+  activeNote: any;
+  setActiveNote: any;
+}
+
+const AllNotes = (props: AllNotesProps) => {
+  const sortedNotes = props.notes.sort(
+    (a: any, b: any) => b.lastModified - a.lastModified
+  );
+
+
+  return (
+    <div className="main">
+      <div className="main__header">
+        <button onClick={props.onAddNote}>Создать</button>
+      </div>
+      <div className="main__notes">
+        {sortedNotes.map(
+          ({
+            id,
+            title,
+            body,
+            lastModified,
+          }: {
+            id: string;
+            title: string;
+            body: any;
+            lastModified: number;
+          }) => (
+            <div
+              className={`main__note ${
+                id === props.activeNote && 'active'
+              }`}
+              onClick={() => props.setActiveNote(id)}
+              key={id}
+            >
+              <div className="main__note_title">
+                <strong>{title}</strong>
+                <button onClick={() => props.onDeleteNote(id)}>Удалить</button>
+              </div>
+
+              <p className="main__note_body">{body}</p>
+              <small className="note-meta">
+                Последние изменения{' '}
+                {new Date(lastModified).toLocaleDateString('ru-RU', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}
+              </small>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllNotes;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,14 +3,57 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import './App.css';
 import '../../index.css'
+import AllNotes from '../AllNotes';
+import { useState } from 'react';
+
+import uuid from 'react-uuid';
+
+type NewNote = {
+  id: string;
+  title: any;
+  body: string;
+  lastModified: number;
+};
 
 const App: React.FC = () => {
-  //const [notes, setNotes] = useState([]);
-  //const [activeNote, setActiveNote] = useState('');
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+  const [activeNote, setActiveNote] = useState('');
+  console.log(activeNote);
+
+  const onAddNote = () => {
+    const newNote: NewNote = {
+      id: uuid(),
+      title: '',
+      body: '',
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+    if (newNote.id) {
+      setActiveNote(newNote.id);
+    }
+
+    if (newNote.title == '') {
+      newNote.title = Date.now();
+    }
+  };
+
+  const onDeleteNote = (noteId: any) => {
+    setNotes(notes.filter(({ id }: { id: any }) => id !== noteId));
+  };
 
   return (
     <div className='app'>
       <Header />
+      <AllNotes
+        notes={notes}
+        onDeleteNote={onDeleteNote}
+        onAddNote={onAddNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
       <Footer />
     </div>
   );
