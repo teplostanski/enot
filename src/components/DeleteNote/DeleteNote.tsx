00e39c6cd5517777247ea96e05375b27f96f0055
teplostanski <teplostanski@yandex.ru>
2023-03-03 10:13:03 +0300

    disable eslint @typescript-eslint/no-explicit-any

--- src/components/DeleteNote/DeleteNote.tsx
+++ src/components/DeleteNote/DeleteNote.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+//import React from 'react';
 //import Markdown from 'react-markdown';
 
 interface DeleteNoteProps {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import './Header.css';
 import logoSrc from '../../assets/logo.png';
 
