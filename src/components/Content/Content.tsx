2eb867372f38ffc94f1bf38c67e3c764f34ff98d
teplostanski <teplostanski@yandex.ru>
2023-03-07 10:20:44 +0300

    change markdown preview

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Markdown from 'react-markdown';
 
 import './Content.css';
+import remarkGfm from 'remark-gfm';
+
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import vs from 'react-syntax-highlighter/dist/esm/styles/prism/dracula';
 
 interface ContentProps {
   activeNote: any;
@@ -50,9 +54,33 @@ const Content = (props: ContentProps) => {
       </div>
       <div className="content__note_preview">
         <h1 className="preview-title">{props.activeNote.title}</h1>
-        <Markdown className="markdown-preview">
-          {props.activeNote.body}
-        </Markdown>
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          children={props.activeNote.body}
+          className="markdown-preview"
+          components={{
+            code({ inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  {...props}
+                  children={String(children).replace(/\n$/, '')}
+                  style={vs}
+                  language={match[1]}
+                  PreTag="div"
+                  showLineNumbers= {true}
+                  customStyle={{borderRadius: "0", backgroundColor: "#24292e"}}
+                />
+              ) : (
+                <code {...props} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        />
+        {/*{}
+        </Markdown>*/}
       </div>
     </div>
   );
